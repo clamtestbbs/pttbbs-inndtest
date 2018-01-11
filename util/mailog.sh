920af6ac81c290e4da5a7e1d5ccd72f7fe19fd7c
holishing <holishingtest@gmail.com>
2018-01-11 17:29:20 +0800

    mailog: don't log deprecated spam utils

--- util/mailog.sh
+++ util/mailog.sh
@@ -3,7 +3,7 @@
 #
 # 整理出廣告信名單
 
-bin/antispam 
-bin/post Record 今日違法廣告信名單 [Ptt警察局] etc/spam
+#bin/antispam 
+#bin/post Record 今日違法廣告信名單 [Ptt警察局] etc/spam
 bin/post Security 站外來信紀錄mailog [系統安全局] etc/mailog
-rm etc/mailog
+#rm etc/mailog
